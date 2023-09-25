c6c13ce43676649aac6a000bbe15b5d7a756865b
Marwan <marwaneg@aucegypt.edu>
2023-09-25 17:30:13 +0300

    Add footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,33 +5,37 @@ import { CustomButton } from ".";
 
 const Hero = () => {
   const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-  }
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, or rent — quickly abd easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
+
         <p className="hero__subtitle">
           Streamline your car rental experience with our effortless booking
           process.
         </p>
-        <CustomButton 
+
+        <CustomButton
           title="Explore Cars"
-          containerStyles="bg-primary-blue 
-          text-white rounded-full mt-10"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
-          <div className="hero__image-overlay">
-
-          </div>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
